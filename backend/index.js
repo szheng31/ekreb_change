ce07f5c9c2384f4ab26bf0b5b88bedfcdd5fef6d
Stanley Zheng <szheng31@stuy.edu>
2023-09-22 11:40:17 -0500

    added game

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+
+const axios = require('axios')
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,7 +8,35 @@ const PORT = 3000; // May need to change PORT to something else if 3000 is alrea
 app.use(cors());
 
 let score = 0;
+let counter = 0;
+let word = "";
 
+async function getWord(word) {
+    let request = "";
+    await axios.get("https://random-word-api.herokuapp.com/word")
+    .then((response) => request = response.data[0])
+    .catch((err) => console.log(err));
+    return request;
+}
+
+async function getHint(word) {
+    const options = {
+        method: 'GET',
+        url: `https://wordsapiv1.p.rapidapi.com/words/${word}/definitions`,
+        headers: {
+          'X-RapidAPI-Key': 'efb99507e2msh407efe5f14c5ce9p140be1jsn91d4caa7a5bc',
+          'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+        }
+      };
+      
+      try {
+          const response = await axios.request(options); return response.data;
+          
+      } catch (error) {
+          console.error(error);
+      }
+      
+}
 app.get('/hello', (req, res) => {
     res.send('hello world!');
 });
@@ -20,12 +50,56 @@ app.patch('/score', (req, res) => {
     res.status(200).send(`${score}`);
 })
 
-app.get('/getWord', (req, res) => {
-    res.send('world');
+app.get('/getWord', async (req, res) => {
+    const request = await getWord();
+        
+    
+    console.log("request: " + request);
+
+    res.send(request);
+    
+    
 })
 
+
+app.get('/saveWord', (req,res) => {
+    
+    let q = req.query.word;
+    console.log("recieved saveWord: " + q);
+    word = q;
+})
+
+app.get('/word', (req,res) => {
+    res.send(word);
+})
+
+app.get('/getHint', async (req,res) => {
+    console.log("word: " + word);
+    const requestHint = await getHint(word);
+
+    res.send(requestHint);
+})
+
+app.get('/scrambleWord', (req,res) => {
+    let q = req.query.word;
+    
+    let letters = q.split("");
+    let scrambledWord = "";
+    while (letters.length > 0) {
+        randIndex = Math.floor(Math.random() * (letters.length));
+        scrambledWord += letters[randIndex];
+        letters.splice(randIndex,1);
+    }
+    
+    console.log("scrambleword:" + word);
+    res.send(scrambledWord);
+ })
+
+
 app.patch('/guessWord', (req, res) => {
-    if(req.query.word === 'world') {
+    console.log(req.query.word);
+    console.log("currentWord: " + word);
+    if(req.query.word === word) {
         res.status(200).send('true');
     } else {
         res.status(200).send('false');
--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -2,28 +2,150 @@ import { useState, useEffect } from 'react';
 import { Input, Button, notification } from 'antd';
 import axios from 'axios';
 
-function Game () {
 
-    const [word, setWord] = useState("hello");
+
+function Game () {
+    
+    
+    const [word, setWord] = useState("");
     const [score, setScore] = useState(0);
     const [guess, setGuess] = useState("");
+    const [hint,setHint] = useState("N/A");
+    const [scramWord, setScramWord] = useState("");
+    const [sec, setSec] = useState(5);
 
     useEffect(() => { 
+        const interval = setInterval(() => setSec(sec-1),1000);
+        
         let config = {
             method: 'get',
             maxBodyLength: Infinity,
             url: 'http://localhost:3000/getWord',
             headers: { }
           };
-          
-          axios.request(config)
+        
+        if (word == "") {
+            console.log("hi")
+            axios.request(config)
             .then((response) => {
                 setWord(response.data);
+                getScrambled(response.data);
+                saveWord(response.data);
             })
             .catch((error) => {
                 console.log(error);
             });
+            
+        }
+        gameOver();
+        return () => clearInterval(interval);
+        
     });
+
+    
+    const gameOver = () => {
+        var hide = document.getElementById("play");
+        var stats = document.getElementById("stats");
+        if (sec <= 0) {
+            hide.style.display = "none";
+            stats.style.display = "block";
+        } 
+        else {
+            var hide = document.getElementById("play");
+            hide.style.display = "block";
+            stats.style.display = "none";
+        }
+    
+    }
+    
+    const skipWord = () => {
+        
+        setWord("");
+        
+        setScramWord("");
+
+        var x = document.getElementById("hints");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        }
+    }
+
+    const restartGame = () => {
+
+        setWord("");
+        setScore(0);
+        setGuess("");
+        setHint("N/A");
+        setScramWord("");
+        setSec(60);
+
+        console.log("restartGame")
+    }
+    
+    const saveWord = (word) => {
+
+        let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/saveWord?word=' + word,
+        headers: { }
+        };
+
+        axios.request(config)
+            .then((response) => {
+                
+            })
+            .catch((error) => {
+                console.log(error);
+        });
+    }
+
+    const getScrambled = (word) => {
+
+        let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/scrambleWord?word=' + word,
+        headers: { }
+        };
+
+        axios.request(config)
+            .then((response) => {
+                setScramWord(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+        });
+    }
+
+    const getHint = () => {
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:3000/getHint',
+            headers: { }
+          };
+          
+          axios.request(config)
+          .then((response) => {
+            var x = document.getElementById("hints");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+
+
+            
+            
+            
+            setHint(response.data['definitions'][0]['definition']);
+            console.log("data: " + response.data['definitions'][0]['definition']);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }
 
     const handleSubmit = () => {
         let config = {
@@ -43,30 +165,59 @@ function Game () {
                         placement: "bottomRight",
                         duration: 2
                     });
+                    setWord("");
+                    setScramWord("");
+                    setHint("N/A");
+                    var x = document.getElementById("hints");
+                    if (x.style.display === "block") {
+                        x.style.display = "none";
+                    }
                 } else {
                     notification.error({
-                        message: "Inorrect!",
+                        message: "Incorrect!",
                         description: "You guessed the word incorrectly!",
                         placement: "bottomRight",
                         duration: 2
                     });
                 }
                 setGuess("");
+                
             })
             .catch((error) => {
                 console.log(error);
             });
     };
 
-    return <div className="card">
+    const hints = {
+        display: 'none'
+    }
+    
+
+    return <div className="card" >
+
+        <div id="play">
+       <h1>{sec}</h1>
         <h2> Current Word: {word} </h2>
+        <h2> Scrambled Word: {scramWord} </h2>
         <Input size="large" placeholder="Enter your guess"
             onChange={(input) => {setGuess(input.target.value); }}
             value={guess} />
         <br /> <br />
+
+
         <Button type="primary" size="large" onClick={handleSubmit}>Submit</Button>
+        <Button type="primary" size="large" onClick={getHint}>Get Hint</Button>
+        <Button type="primary" size="large" onClick={skipWord}>Skip Word</Button>
+        <h4  style={hints} id="hints" >Definition: {hint}</h4>
         <p> Score: {score} </p>
     </div>
+
+    <div id="stats">
+        <h1>Final Score: {score} </h1>
+        <Button type="primary" size="large" onClick={restartGame}> Start Over? </Button>
+    </div>
+    </div>
+        
     
     
 }
